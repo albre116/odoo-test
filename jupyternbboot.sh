a5179ed88c4be1921827c5ae649048e6899f20d6
root <you@example.com>
2017-09-18 02:03:32 +0000

    ok good instructions

--- jupyternbboot.sh
+++ jupyternbboot.sh
@@ -2,4 +2,4 @@
 docker run -d -p 8888:8888 \
 --user root -e GRANT_SUDO=yes \
 -v $(pwd):/home/jovyan/work \
-jupyter/datascience-notebook start-notebook.sh --NotebookApp.token='sha1:ef32e09de243:e62e989f13b071cdf2558425c3838916cce6a5c4'
+jupyter/datascience-notebook start-notebook.sh --NotebookApp.token='EzAce'
