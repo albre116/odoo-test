73952583ecd01ab0fead083ed2588cac642b4c94
root <you@example.com>
2017-09-18 02:49:17 +0000

    night's work

--- misc-addons/ez_ace_test/__manifest__.py
+++ misc-addons/ez_ace_test/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/ez_ace_test.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
