e4549e3004a444c7e0dfd405e424e173b32c8b4d
root <you@example.com>
2017-09-18 01:42:19 +0000

    huh

--- build.sh
+++ build.sh
@@ -34,8 +34,6 @@ docker run hello-world
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-### Get the source code
-git clone https://github.com/albre116/SocialMedia.git
 
 
 
