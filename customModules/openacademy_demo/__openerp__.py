d3ec04defb6a60a12e29737347e181a4cb52c940
Your Name <you@example.com>
2017-09-21 03:27:40 +0000

    added test module

---
+++ customModules/openacademy_demo/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Covantec R.L.",
+    'website': "https://coderwall.com/team/covantec",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web_gantt'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
